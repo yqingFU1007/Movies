93f3536d8688adff19b756b399ed316a732acfc8
浅枫 <767038756@qq.com>
2018-05-02 18:30:05 +0800

    default

--- app/src/main/java/com/example/asa/movies/MainActivity.java
+++ app/src/main/java/com/example/asa/movies/MainActivity.java
@@ -23,7 +23,7 @@ import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
 
-    String URL_BASE = "https://api.themoviedb.org/3/movie/popular?api_key=cee990f8787ceedaa4db6a9496a53c45";
+    String URL_BASE = "https://api.themoviedb.org/3/movie/popular?api_key=[]";
 
     HomepageAdapter mAdapter;
     GridView gridView;
@@ -38,8 +38,7 @@ public class MainActivity extends AppCompatActivity {
         new NetworkTask().execute(URL_BASE);
 
 
-
-        gridView = (GridView)findViewById(R.id.grid_view);
+        gridView = (GridView) findViewById(R.id.grid_view);
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -52,16 +51,16 @@ public class MainActivity extends AppCompatActivity {
                 String title = movieCurrent.getmMovieName();
                 String score = movieCurrent.getmVoteAverage();
                 String date = movieCurrent.getmReleaseDate();
-                Log.e("MainActivity",movieCurrent.getmReleaseDate() + "");
-                Log.e("MainActivity",movieCurrent.getmOverView() + "");
+                Log.e("MainActivity", movieCurrent.getmReleaseDate() + "");
+                Log.e("MainActivity", movieCurrent.getmOverView() + "");
 
-                Intent intent = new Intent(MainActivity.this,DetailActivity.class);
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
                 Bundle bundle = new Bundle();
-                bundle.putString("posterSource",posterSource);
-                bundle.putString("overView",overView);
-                bundle.putString("title",title);
-                bundle.putString("score",score);
-                bundle.putString("date",date);
+                bundle.putString("posterSource", posterSource);
+                bundle.putString("overView", overView);
+                bundle.putString("title", title);
+                bundle.putString("score", score);
+                bundle.putString("date", date);
 
                 intent.putExtras(bundle);
                 startActivity(intent);
@@ -71,20 +70,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private class NetworkTask extends AsyncTask<String,Void,ArrayList<MovieInfo>>{
+    private class NetworkTask extends AsyncTask<String, Void, ArrayList<MovieInfo>> {
 
         @Override
         protected ArrayList<MovieInfo> doInBackground(String... strings) {
 
             mMovieInfos = Utils.parseJSONResponse(strings[0]);
-            Log.e("MainActivity",mMovieInfos.get(0).getmMovieName() + "");
-            return  mMovieInfos;
+            Log.e("MainActivity", mMovieInfos.get(0).getmMovieName() + "");
+            return mMovieInfos;
         }
 
         @Override
         protected void onPostExecute(ArrayList<MovieInfo> movieInfos) {
-            mAdapter = new HomepageAdapter(MainActivity.this,movieInfos);
-            Log.e("MainActivity","" + mAdapter);
+            mAdapter = new HomepageAdapter(MainActivity.this, movieInfos);
+            Log.e("MainActivity", "" + mAdapter);
             gridView.setAdapter(mAdapter);
         }
     }
@@ -92,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main,menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -100,43 +99,41 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int menuItemThatWasSelected = item.getItemId();
         Comparator<MovieInfo> c;
-        if (menuItemThatWasSelected == R.id.action_sort_by_score){
+        if (menuItemThatWasSelected == R.id.action_sort_by_score) {
             c = new Comparator<MovieInfo>() {
                 @Override
                 public int compare(MovieInfo movieInfo, MovieInfo t1) {
-                    if (Double.parseDouble(movieInfo.getmVoteAverage()) > Double.parseDouble(t1.getmVoteAverage())){
+                    if (Double.parseDouble(movieInfo.getmVoteAverage()) > Double.parseDouble(t1.getmVoteAverage())) {
                         return -1;
-                    }
-                    else return 1;
+                    } else return 1;
                 }
             };
-            Collections.sort(mMovieInfos,c);
-            for (int i = 0;i < mMovieInfos.size();i++){
-                Log.e("MainActivity",mMovieInfos.get(i).getmVoteAverage());
-                Log.e("MainActivity",mMovieInfos.get(i).getmPosterUrlString());
+            Collections.sort(mMovieInfos, c);
+            for (int i = 0; i < mMovieInfos.size(); i++) {
+                Log.e("MainActivity", mMovieInfos.get(i).getmVoteAverage());
+                Log.e("MainActivity", mMovieInfos.get(i).getmPosterUrlString());
             }
 
-            mAdapter = new HomepageAdapter(MainActivity.this,mMovieInfos);
+            mAdapter = new HomepageAdapter(MainActivity.this, mMovieInfos);
             gridView.setAdapter(mAdapter);
         }
 
-        if (menuItemThatWasSelected == R.id.action_sort_by_popularity){
+        if (menuItemThatWasSelected == R.id.action_sort_by_popularity) {
             c = new Comparator<MovieInfo>() {
                 @Override
                 public int compare(MovieInfo movieInfo, MovieInfo t1) {
-                    if (Double.parseDouble(movieInfo.getmPopularity()) > Double.parseDouble(t1.getmPopularity())){
+                    if (Double.parseDouble(movieInfo.getmPopularity()) > Double.parseDouble(t1.getmPopularity())) {
                         return -1;
-                    }
-                    else return 1;
+                    } else return 1;
                 }
             };
-            Collections.sort(mMovieInfos,c);
-            for (int i = 0;i < mMovieInfos.size();i++){
-                Log.e("MainActivity",mMovieInfos.get(i).getmPopularity());
-                Log.e("MainActivity",mMovieInfos.get(i).getmPopularity());
+            Collections.sort(mMovieInfos, c);
+            for (int i = 0; i < mMovieInfos.size(); i++) {
+                Log.e("MainActivity", mMovieInfos.get(i).getmPopularity());
+                Log.e("MainActivity", mMovieInfos.get(i).getmPopularity());
             }
 
-            mAdapter = new HomepageAdapter(MainActivity.this,mMovieInfos);
+            mAdapter = new HomepageAdapter(MainActivity.this, mMovieInfos);
             gridView.setAdapter(mAdapter);
         }
 
